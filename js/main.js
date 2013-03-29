c314207468a8a4e87d99ad03eafec637cc55e48f
Sergio Morstabilini <mosta.pb@gmail.com>
2013-03-29 19:08:03 +0100

    part 3

--- js/main.js
+++ js/main.js
@@ -12,11 +12,22 @@ var app = {
             }
         });
     },
-
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
     initialize: function() {
         // this.store = new MemoryStore();
         // this.store = new LocalStorageStore();
-        this.store = new WebSqlStore();
+        var self = this;
+        
+        // this.store = new WebSqlStore();
+        this.store = new WebSqlStore(function(){
+            self.showAlert('Store initialized', 'My title');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 

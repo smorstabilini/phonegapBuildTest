84b09bfa530cde43acc192cb8c5e1e963f481deb
Sergio Morstabilini <mosta.pb@gmail.com>
2013-03-30 14:43:48 +0100

    part 9

--- js/main.js
+++ js/main.js
@@ -7,21 +7,54 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    initialize: function() {
-        //this.store = new MemoryStore();
-        // this.store = new LocalStorageStore();
+    registerEvents: function() {
         var self = this;
-        
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        };
+        $(window).on('hashchange', $.proxy(this.route, this));
+    },
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+    initialize: function() {
+        var self = this;
+        this.registerEvents();
+        this.detailsURL = /^#employees\/(\d{1,})/;
         // this.store = new WebSqlStore();
         //this.store = new WebSqlStore(function(){
         this.store = new MemoryStore(function(){
             // self.showAlert('Store initialized', 'My title');
             // self.renderHomeView();
-            $('body').html(new HomeView(self.store).render().el);
+            // $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
+        
     }
 
 };
